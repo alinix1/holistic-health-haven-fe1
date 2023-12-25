6701956963fa960325ab624a86969e8cb4e8122f
Ali <anix1@kent.edu>
2023-12-25 16:19:01 -0500

    add mockData file and set up apiCalls file and update App component

--- src/apiCalls.ts
+++ src/apiCalls.ts
@@ -1,1 +1,10 @@
-export {};
+import holisticProductData from "./mockData";
+
+const getHolisticProducts = () => {
+  return Promise.resolve({
+    json: () => Promise.resolve(holisticProductData),
+    ok: true,
+  });
+};
+
+export { getHolisticProducts };
