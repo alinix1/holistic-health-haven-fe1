42a2e5a476632348d8e4e3903beafd454fdd056c
Ali <anix1@kent.edu>
2024-01-03 19:51:37 -0700

    add Footer and Tailwind styling

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,1 +1,17 @@
-export {};
+const Footer = () => {
+  return (
+    <footer className="footer-container flex items-center justify-center h-13.5vh w-screen p-8 bg-[#BFB2A3]">
+      <span className="footer-text italic text-lg">
+        &copy; 2024 by{" "}
+        <a
+          href="https://github.com/alinix1/holistic-health-haven-fe1"
+          className="text-[#402B18] hover:underline"
+        >
+          Ali Nix
+        </a>
+        . All rights reserved.
+      </span>
+    </footer>
+  );
+};
+export default Footer;
