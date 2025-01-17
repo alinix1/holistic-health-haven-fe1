66c4b0707a14911e823e745f92a3e14a2219a8d8
Ali <anix1@kent.edu>
2025-01-17 15:34:26 -0700

    enhance Dropdown to display selected ailment and reset on 'Select Ailment'

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -7,8 +7,12 @@ import type { DropdownProps } from "../../model";
 //   handleAilmentSelect: (selectedAilment: string) => void;
 // }
 
-const Dropdown: React.FC<DropdownProps> = ({ handleAilmentSelect }) => {
+const Dropdown: React.FC<DropdownProps> = ({
+  handleAilmentSelect,
+  ailment,
+}) => {
   const ailments = [
+    "Select Ailment",
     "Fibromyalgia",
     "Stress & Anxiety",
     "Insomnia",
@@ -26,22 +30,26 @@ const Dropdown: React.FC<DropdownProps> = ({ handleAilmentSelect }) => {
       {/* Dropdown Button */}
 
       <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm ring-1 ring-gray-300 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500">
-        Select Ailment
+        {ailment || "Select Ailment"}
         <ChevronDownIcon aria-hidden="true" className="w-5 h-5 text-gray-500" />
       </MenuButton>
       {/* Dropdown Items */}
       <MenuItems className="absolute left-0 z-10 mt-2 w-56 origin-top-left rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
-        {ailments.map((ailment) => (
-          <MenuItem key={ailment}>
+        {ailments.map((ailmentOption) => (
+          <MenuItem key={ailmentOption}>
             {({ active }) => (
               <button
                 type="button"
-                onClick={() => handleAilmentSelect(ailment)}
+                onClick={() =>
+                  handleAilmentSelect(
+                    ailmentOption === "Select Ailment" ? "" : ailmentOption
+                  )
+                }
                 className={`${
                   active ? "bg-gray-100 text-gray-900" : "text-gray-700"
                 } block w-full text-left px-4 py-2 text-sm`}
               >
-                {ailment}
+                {ailmentOption}
               </button>
             )}
           </MenuItem>
