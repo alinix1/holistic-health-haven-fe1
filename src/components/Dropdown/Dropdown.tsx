020e06e9d71fcfa28f8460bd50095542610e1ef1
Ali <anix1@kent.edu>
2025-01-17 14:47:30 -0700

    installs headless ui and creates a more stylish dropdown menu

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,4 +1,6 @@
 import type React from "react";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import type { DropdownProps } from "../../model";
 
 // interface DropdownProps {
@@ -20,19 +22,32 @@ const Dropdown: React.FC<DropdownProps> = ({ handleAilmentSelect }) => {
   ];
 
   return (
-    <form className="dropdown-form">
-      <select
-        name="ailments ml-2 h-20vh w-30vw"
-        onChange={(event) => handleAilmentSelect(event.target.value)}
-      >
-        <option value="">Select Ailment</option>
+    <Menu as="div" className="relative inline-block text-left">
+      {/* Dropdown Button */}
+
+      <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm ring-1 ring-gray-300 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500">
+        Select Ailment
+        <ChevronDownIcon aria-hidden="true" className="w-5 h-5 text-gray-500" />
+      </MenuButton>
+      {/* Dropdown Items */}
+      <MenuItems className="absolute left-0 z-10 mt-2 w-56 origin-top-left rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
         {ailments.map((ailment) => (
-          <option key={ailment} value={ailment}>
-            {ailment}
-          </option>
+          <MenuItem key={ailment}>
+            {({ active }) => (
+              <button
+                type="button"
+                onClick={() => handleAilmentSelect(ailment)}
+                className={`${
+                  active ? "bg-gray-100 text-gray-900" : "text-gray-700"
+                } block w-full text-left px-4 py-2 text-sm`}
+              >
+                {ailment}
+              </button>
+            )}
+          </MenuItem>
         ))}
-      </select>
-    </form>
+      </MenuItems>
+    </Menu>
   );
 };
 
