517eee00d63d2edfe658cb4499b6a9e60fbd868e
Ali <anix1@kent.edu>
2025-01-17 23:34:49 -0700

    creates AboutPage component and adds link to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -67,9 +67,17 @@ const Header: React.FC<HeaderProps> = ({ children, cartItems }) => {
             Payment
           </button>
         </Link>
+        <Link to="/about" className="ml-4 font-extrabold">
+          <button
+            type="button"
+            className=" nav-button rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-black transition duration-150 ease-in-out hover:bg-[#D9C4B2] hover:text-[#402B18] focus:text-[#402B18] focus:outline-none focus:ring-0 active:text-[#8C6645]"
+          >
+            About
+          </button>
+        </Link>
       </nav>
       <section>
-        <h1 className="text-black font-regular text-4xl mb-4">
+        <h1 className="text-black text-4xl font-bold mb-4">
           Holistic Health Haven
         </h1>
         <article className="mt-12 flex">
