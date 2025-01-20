0da7308b0b9a1c5e14ccd025d0fa37b4a1af3eba
Ali <anix1@kent.edu>
2025-01-20 16:04:59 -0700

    adds terms & conditions, privacy policy links as buttons to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,6 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
     <footer className="footer-container flex items-center justify-center h-13.5vh w-screen p-8 bg-[#BFB2A3]">
@@ -11,6 +14,22 @@ const Footer = () => {
         </a>
         . All rights reserved.
       </span>
+      <Link to="/terms-and-conditions" className="ml-4 font-extrabold">
+        <button
+          type="button"
+          className="nav-button rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-black transition duration-150 ease-in-out hover:bg-[#D9C4B2] hover:text-[#402B18] focus:text-[#402B18] focus:outline-none focus:ring-0 active:text-[#8C6645]"
+        >
+          Terms & Conditions
+        </button>
+      </Link>
+      <Link to="/privacy-policy" className="ml-4 font-extrabold">
+        <button
+          type="button"
+          className="nav-button rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-black transition duration-150 ease-in-out hover:bg-[#D9C4B2] hover:text-[#402B18] focus:text-[#402B18] focus:outline-none focus:ring-0 active:text-[#8C6645]"
+        >
+          Privacy Policy
+        </button>
+      </Link>
     </footer>
   );
 };
