63c9848412723f18edf6a449902a162f3f6ab25f
alinix1 <anix1@kent.edu>
2025-01-31 00:15:37 -0700

    modifies api calls to include async/await with axios

--- src/apiCalls.ts
+++ src/apiCalls.ts
@@ -2,24 +2,24 @@ import axios from "axios";
 
 const API_BASE_URL = "http://localhost:9000/api/v1";
 
-const getHolisticProducts = () => {
-  return axios
-    .get(`${API_BASE_URL}/products`)
-    .then((response) => response.data)
-    .catch((error) => {
-      console.error("Error fetching holistic products", error);
-      throw error;
-    });
+const getHolisticProducts = async () => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/products`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching holistic products", error);
+    throw error;
+  }
 };
 
-const getReviews = () => {
-  return axios
-    .get(`${API_BASE_URL}/reviews`)
-    .then((response) => response.data)
-    .catch((error) => {
-      console.error("Error fetching reviews", error);
-      throw error;
-    });
+const getReviews = async () => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/reviews`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching reviews", error);
+    throw error;
+  }
 };
 
 export { getHolisticProducts, getReviews };
