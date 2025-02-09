eb706037cc70c15a2c02fa0e57111ebfc67b98ea
alinix1 <anix1@kent.edu>
2025-02-08 19:06:23 -0700

    wraps App with CartProvider

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./components/App/App";
+import { CartProvider } from "./contexts/CartContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </React.StrictMode>
   </BrowserRouter>,
 );
