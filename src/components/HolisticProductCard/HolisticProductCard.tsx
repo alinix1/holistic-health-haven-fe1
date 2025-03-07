14f11495025a14a71ee7e9c8faf3b2a8819c5e0d
alinix1 <anix1@kent.edu>
2025-03-06 17:14:16 -0700

    update typography for title and product type

--- src/components/HolisticProductCard/HolisticProductCard.tsx
+++ src/components/HolisticProductCard/HolisticProductCard.tsx
@@ -14,13 +14,15 @@ const HolisticProductCard: React.FC<HolisticProductProps> = ({
         className="product-card box-border flex flex-col items-center gap-7 m-4 p-4 px-8 w-3/4 h-auto rounded-lg shadow-xl bg-white overflow-hidden"
         key={id}
       >
-        <h1 className="product-title mt-3 font-bold">{product_title}</h1>
+        <h1 className="product-title mt-3 font-inter text-2xl font-bold">
+          {product_title}
+        </h1>
         <img
           className="product-img w-80 h-48 object-cover rounded-lg shadow-none transition-shadow duration-500 ease-in-out hover:shadow-4-strong"
           src={img}
           alt={product_title}
         />
-        <p className="product-type mb-5 text-opacity-70">
+        <p className="product-type font-jakarta font-semibold mb-5">
           {product_type.join(", ")}
         </p>
       </div>
