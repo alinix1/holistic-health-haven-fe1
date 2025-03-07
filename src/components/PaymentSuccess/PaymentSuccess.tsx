102463a36df16aecc904077a99d0e42a0fd00d55
alinix1 <anix1@kent.edu>
2025-03-06 17:08:52 -0700

    update typography for consistent fonts

--- src/components/PaymentSuccess/PaymentSuccess.tsx
+++ src/components/PaymentSuccess/PaymentSuccess.tsx
@@ -4,11 +4,13 @@ import Button from "../Button/Button";
 const PaymentSuccess: React.FC = () => {
   return (
     <div className="min-h-screen bg-[#402B18] flex flex-col items-center justify-center space-y-8">
-      <h1 className="text-3xl text-white font-bold">
+      <h1 className="text-3xl text-white font-inter text-4xl font-bold">
         Payment Success!
         <span className="inline-block ml-2 animate-bounce">🎉</span>{" "}
       </h1>
-      <p className="text-white text-normal">Thank you for your purchase.</p>
+      <p className="text-white text-normal font-inter">
+        Thank you for your purchase.
+      </p>
       <Link to="/" className="bottom-0 left-0">
         <Button className="mt-4 md:mt-0 lowercase">Home</Button>
       </Link>
