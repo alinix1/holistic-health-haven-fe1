7a9883da87dcfd6a5193bad826e82e69e4d2d484
alinix1 <anix1@kent.edu>
2025-03-10 14:10:37 -0600

    remove overflow-hidden and adjust image spacing for responsive styling

--- src/components/CarouselSection/CarouselSection.tsx
+++ src/components/CarouselSection/CarouselSection.tsx
@@ -5,12 +5,12 @@ import sage from "../../assets/sage.jpg";
 import yoga from "../../assets/yoga.jpg";
 const CarouselSection: React.FC = () => {
   return (
-    <div className="relative w-full overflow-hidden flex items-center bg-slate-800 h-56 sm:h-64 xl:h-80 2xl:h-96">
+    <div className="relative w-full flex items-center bg-slate-800 h-56 sm:h-64 xl:h-80 2xl:h-96">
       <Carousel>
         <img
           src={aboutProduct}
           alt="about product"
-          className="absolute  w-full md:w-[50rem] h-auto object-cover -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+          className="absolute w-full md:w-[50rem] h-auto object-cover -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
         />
         <img
           src={sage}
