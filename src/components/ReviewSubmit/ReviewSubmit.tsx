c239137c36550deb2456059fd30f4eef427a3e42
alinix1 <anix1@kent.edu>
2025-04-07 15:12:01 -0600

    replace useHistory with useNavigate for ReviewSubmit component

--- src/components/ReviewSubmit/ReviewSubmit.tsx
+++ src/components/ReviewSubmit/ReviewSubmit.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import ReviewForm from "../../pages/ReviewForm";
 import { postReview } from "../../apiCalls";
 import { Review } from "../../resources/model";
 
 const ReviewSubmit: React.FC = () => {
-  const history = useHistory();
+  const navigate = useNavigate();
 
   const handleReviewSubmit = async (values: Review): Promise<void> => {
     try {
       await postReview(values);
-      history.push("/testimonials");
+      navigate("/testimonials");
     } catch (error) {
       console.error("Error submitting review", error);
     }
