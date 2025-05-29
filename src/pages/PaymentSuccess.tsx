b7e353dbaf206fec69f7a6a22cadffae132be9b5
alinix1 <anix1@kent.edu>
2025-05-29 17:42:49 -0600

    clean up PaymentSuccess styling and structure

--- src/pages/PaymentSuccess.tsx
+++ src/pages/PaymentSuccess.tsx
@@ -3,16 +3,18 @@ import { Link } from "react-router-dom";
 import Button from "../components/Button/Button";
 const PaymentSuccess: React.FC = () => {
   return (
-    <div className="min-h-screen bg-[#f1ede5] flex flex-col items-center justify-center space-y-8">
-      <h1 className="text-3xl text-black font-inter text-4xl font-bold">
-        Payment Success!
-        <span className="inline-block ml-2 animate-bounce">🎉</span>{" "}
-      </h1>
-      <p className="text-black text-normal font-inter">
-        Thank you for your purchase.
-      </p>
-      <Link to="/" className="bottom-0 left-0">
-        <Button className="mt-4 md:mt-0 lowercase">Home</Button>
+    <div className="flex min-h-screen flex-col items-center justify-center space-y-6 bg-[#f1ede5]">
+      <div className="flex flex-col items-center space-y-4">
+        <h1 className="font-inter text-4xl font-bold text-black">
+          Payment Success!
+          <span className="ml-2 inline-block animate-bounce">🎉</span>{" "}
+        </h1>
+        <p className="font-inter text-base text-black">
+          Thank you for your purchase.
+        </p>
+      </div>
+      <Link to="/">
+        <Button className="lowercase">Home</Button>
       </Link>
     </div>
   );
