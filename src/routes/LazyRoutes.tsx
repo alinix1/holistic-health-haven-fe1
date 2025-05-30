6451c8ecb267e6e6851035e0568ebce4b6162e5c
alinix1 <anix1@kent.edu>
2025-05-30 15:13:22 -0600

    fix LazyRoutes typo

--- src/routes/LazyRoutes.tsx
+++ src/routes/LazyRoutes.tsx
@@ -1,26 +1,25 @@
-import { lazy } from 'react'; 
+import { lazy } from "react";
 
-// Lazy load all page components 
+// Lazy load all page components
 
-export const HolisticProductList = lazy(() => 
-import('../pages/HolisticProducList')); 
-export const HolisticProductPage = lazy(() => 
-import('../pages/HolisticProductPage')); 
-export const TestimonialsPage = lazy(() => 
-import('../pages/TestimonialsPage')); 
-export const ReviewSubmit = lazy(() => 
-import('../components/ReviewSubmit/ReviewSubmit')); 
-export const TermsAndConditions = lazy(() => 
-import('../pages/TermsAndConditions')); 
-export const PrivacyPolicy = lazy(() => 
-import('../pages/PrivacyPolicy')); 
-export const CheckoutItem = lazy(() => 
-import('../pages/CheckoutItem')); 
-export const PaymentContainer = lazy(() => 
-import('../components/PaymentContainer/PaymentContainer')); 
-export const PaymentSuccess = lazy(() => 
-import('../pages/PaymentSuccess')); 
-export const AboutPage = lazy(() => 
-import('../pages/AboutPage')); 
-export const BadURL = lazy(() => import('../pages/BadURL'));
-
+export const HolisticProductList = lazy(
+  () => import("../pages/HolisticProductList"),
+);
+export const HolisticProductPage = lazy(
+  () => import("../pages/HolisticProductPage"),
+);
+export const TestimonialsPage = lazy(() => import("../pages/TestimonialsPage"));
+export const ReviewSubmit = lazy(
+  () => import("../components/ReviewSubmit/ReviewSubmit"),
+);
+export const TermsAndConditions = lazy(
+  () => import("../pages/TermsAndConditions"),
+);
+export const PrivacyPolicy = lazy(() => import("../pages/PrivacyPolicy"));
+export const CheckoutItem = lazy(() => import("../pages/CheckoutItem"));
+export const PaymentContainer = lazy(
+  () => import("../components/PaymentContainer/PaymentContainer"),
+);
+export const PaymentSuccess = lazy(() => import("../pages/PaymentSuccess"));
+export const AboutPage = lazy(() => import("../pages/AboutPage"));
+export const BadURL = lazy(() => import("../pages/BadURL"));
