f7e9f8f01bfa561b0171b3e83883c52f3d64a97f
Ali Nix <anix8605@gmail.com>
2026-05-11 14:04:30 -0600

    add REACT_APP_BASE_URL and update product image src to use local paths

--- src/components/HolisticProductCard/HolisticProductCard.tsx
+++ src/components/HolisticProductCard/HolisticProductCard.tsx
@@ -11,12 +11,12 @@ const HolisticProductCard: React.FC<HolisticProductProps> = ({
   return (
     <Link to={`/${id}`}>
       <div className="product-card m-4 box-border flex max-w-sm flex-col items-center gap-7 overflow-hidden rounded-lg bg-white p-4 px-8 shadow-2xl sm:min-h-[30rem]">
-        <h1 className="product-title font-inter mt-3 text-2xl font-bold">
+        <h1 className="product-title mt-3 font-inter text-2xl font-bold">
           {product_title}
         </h1>
         <img
           className="product-img h-48 w-80 rounded-lg object-cover shadow-none transition-shadow duration-500 ease-in-out hover:shadow-4-strong"
-          src={img}
+          src={`${process.env.REACT_APP_BASE_URL}${img}`}
           alt={product_title}
           loading="lazy"
         />
